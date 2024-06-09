d85f2b59d2fc6a8edb03607419cbb7f060b89f85
Mokshita144 <104907356+Mokshita144@users.noreply.github.com>
2024-06-09 19:40:56 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+function connect () {
+    mongoose.set('useCreateIndex', true);
+    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true,useUnifiedTopology: true })
+}
+module.exports = connect
