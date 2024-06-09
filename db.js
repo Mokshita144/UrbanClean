407a2e38cecea9b7b875a93ab079f69bfe4e9dc9
Mokshita144 <104907356+Mokshita144@users.noreply.github.com>
2024-06-09 19:25:47 +0530

    Delete db.js

--- db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-function connect () {
-    mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true,useUnifiedTopology: true })
-}
-module.exports = connect
